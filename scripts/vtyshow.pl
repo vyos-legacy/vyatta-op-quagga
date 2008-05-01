8aacffc91a5cab0f2cef616fe31c5616da0da78c
Stephen Hemminger <stephen.hemminger@vyatta.com>
2008-05-01 14:47:39 -0700

    Use wrapper for all show commands
    Rather than convoluted LESSOPEN, use perl wrapper to access vtysh

---
+++ scripts/vtyshow.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+#
+# Module: vtyshow.pl
+# 
+# **** License ****
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# This code was originally developed by Vyatta, Inc.
+# Portions created by Vyatta are Copyright (C) 2007 Vyatta, Inc.
+# All Rights Reserved.
+# 
+# Author: Stephen Hemminger
+# Date: May 2008
+# Description: Script to display quagga information
+# 
+# **** End License ****
+#
+
+use strict;
+use warnings;
+
+exec '/usr/bin/vyatta-vtysh', '-c', join(' ', @ARGV);
+die "Could not exec vtysh";
+
+
