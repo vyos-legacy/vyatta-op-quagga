6e6ceda6fef699a0d1dc4a4992183deca22c561a
root <root@debianlenny.sici.thales.no>
2009-02-12 08:57:43 +0100

    Include translation between Vyatta CLI and Quagga

--- scripts/vtyshow.pl
+++ scripts/vtyshow.pl
@@ -28,7 +28,15 @@ use warnings;
 
 ($ARGV[0] eq 'show' ) or die "must be a show command\n";
 
-exec '/usr/bin/vyatta-vtysh', '-c', join(' ', @ARGV);
+# Upgrade January 2009, Jon Andersson Thales Norway
+# Substitute in argumentlist to keep show commands simple
+my $p = join(' ', @ARGV);
+# Vyatta CLI use ospfv3, Quagga use ospf6, modify
+$p =~ s/ospfv3/ospf6/g; 
+# Quagga use * to denote any, Not possible in Vyatta CLI 
+$p =~ s/ any/ */g;
+
+exec '/usr/bin/vyatta-vtysh', '-c', $p;
 die "Could not exec vtysh";
 
 
